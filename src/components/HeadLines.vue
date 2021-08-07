02c5c0a7945c7cfd673c4bbdcd296d5f4477c03f
AnnabelA1996 <annabelambrosius@outlook.com>
2021-08-07 15:29:17 +1200

    api added and getting data

---
+++ src/components/HeadLines.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    {{ res }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeadLines',
+  data () {
+    return {
+      res: ""
+    }
+
+  },
+  mounted () {
+      this.axios
+      .get('https://newsapi.org/v2/top-headlines?country=us&apiKey=ae6f258d84454d539b607b748ff820bc')
+      .then(response => { this.res = response })
+  },
+  methods () {
+
+  }
+
+}
+</script>
+
+<style>
+
+</style>
